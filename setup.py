06987ab8e4afa539783a3a4b848220ec59c3766e
Enzo Venturi <nzventuri@proton.me>
2023-09-10 20:46:31 -0500

    project name update: 'y2d'
    I've decided to change the project's name to 'y2d' to better describe
    what it does. 'y2d' is a simple command-line tool for creating Anki
    decks from YAML files.

---
+++ setup.py
@@ -0,0 +1,27 @@
+# Copyright 2023 Enzo Venturi. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='y2d',
+    version='0.0.1',
+    description='Generate Anki decks from YAML files or directories',
+    author='Enzo Venturi',
+    author_email='zventuri@tuta.io',
+    url='https://git.sr.ht/~nzv/y2d',
+    packages=find_packages(),
+    install_requires=[
+        'markdown',
+        'pygments',
+        'genanki',
+        'PyYAML',
+        'python-slugify',
+    ],
+    entry_points={
+        'console_scripts': [
+            'y2d = y2d.__main__:main',
+        ],
+    },
+)
